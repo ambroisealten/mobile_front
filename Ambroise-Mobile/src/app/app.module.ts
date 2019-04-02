18937bf874ba866f0d8719aa742390c4f89ca4b4
ThomasDecampAlten <thomas.decamp@alten.com>
2019-04-02 11:43:40 +0200

    Continue comprehension code poc

--- Ambroise-Mobile/src/app/app.module.ts
+++ Ambroise-Mobile/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { TabbarComponent } from './tabbar/tabbar.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { Page3Component } from './page3/page3.component';
+import { ContentPageComponent } from './content-page/content-page.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { Page3Component } from './page3/page3.component';
     TabbarComponent,
     Page1Component,
     Page2Component,
-    Page3Component
+    Page3Component,
+    ContentPageComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +30,6 @@ import { Page3Component } from './page3/page3.component';
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  entryComponents: [TabbarComponent, Page1Component, Page2Component, Page3Component],
+  entryComponents: [ContentPageComponent, SideMenuComponent, TabbarComponent, Page1Component, Page2Component, Page3Component],
 })
 export class AppModule { }
--- Ambroise-Mobile/src/app/tabbar/tabbar.component.ts
+++ Ambroise-Mobile/src/app/tabbar/tabbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Page1Component } from '../page1/page1.component';
+import { Page2Component } from '../page2/page2.component';
+import { Page3Component } from '../page3/page3.component';
 
 @Component({
   selector: 'app-tabbar',
@@ -6,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabbar.component.scss']
 })
 export class TabbarComponent implements OnInit {
-
+  tab1 = Page1Component;
+  tab2 = Page2Component;
+  tab3 = Page3Component;
   constructor() { }
 
   ngOnInit() {
