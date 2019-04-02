69c51451d6587c3a6dd8e8298e238aca8b1be320
ThomasDecampAlten <thomas.decamp@alten.com>
2019-04-02 10:51:51 +0200

    Ajout de 3 pages + Boostrap + Onsen UI

--- Ambroise-Mobile/src/app/app.component.ts
+++ Ambroise-Mobile/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { TabbarComponent } from './tabbar/tabbar.component';
+import { splitAtColon } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  tabbar = TabbarComponent;
   title = 'Ambroise-Mobile';
+  @ViewChild('splitter') splitAtColon;
+
 }
--- Ambroise-Mobile/src/app/app.module.ts
+++ Ambroise-Mobile/src/app/app.module.ts
@@ -7,17 +7,25 @@ import { OnsenModule } from 'ngx-onsenui';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { TabbarComponent } from './tabbar/tabbar.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabbarComponent
+    TabbarComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component
   ],
   imports: [
     BrowserModule,
     OnsenModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  entryComponents: [TabbarComponent, Page1Component, Page2Component, Page3Component],
 })
 export class AppModule { }
