87698dacba0ecbcf53f2b35f8c84e21703f9736c
Camille SCHNELL <camille.schnell@alten.com>
2019-04-02 12:16:57 +0200

    validators for login

--- Ambroise-Mobile/src/app/app.module.ts
+++ Ambroise-Mobile/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -24,7 +24,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(
       appRoutes
     ),
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Ambroise-Mobile/src/app/login/login/login.component.ts
+++ Ambroise-Mobile/src/app/login/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import * as sha512 from 'js-sha512';
 
 @Component({
   selector: 'app-login',
@@ -7,15 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  // used to set validators
+  validatingForm: FormGroup;
+  submitted = false;
+
   userEmail: string;
   userPswd: string;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.validatingForm =this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
   }
 
+  // convenience getter for easy access to form fields
+  get f() { return this.validatingForm.controls; }
+
   onConnect() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.validatingForm.invalid) {
+      return;
+    }
+
+    this.userEmail = this.validatingForm.value.email;
+    this.userPswd = sha512.sha512(this.validatingForm.value.password);
   }
 
 }
