1d9c48d46b4ac5383b82a007e7518a72d3b4131c
ThomasDecampAlten <thomas.decamp@alten.com>
2019-04-03 15:44:34 +0200

    Suppresion ngFor pour passage en dur

--- Ambroise-Mobile/src/app/tabbar/tabbar.component.ts
+++ Ambroise-Mobile/src/app/tabbar/tabbar.component.ts
@@ -4,18 +4,6 @@ import { Page2Component } from '../page2/page2.component';
 import { Page3Component } from '../page3/page3.component';
 import { MenuService } from '../services/MenuService.service';
 
-export class TabLink{
-  name : string;
-  typeIcon : string;
-  page : any;
-
-  constructor(name : string, typeIcon : string, page){
-    this.name = name;
-    this.typeIcon = typeIcon;
-    this.page = page;
-  }
-}
-
 @Component({
   selector: 'ons-page',
   templateUrl: './tabbar.component.html',
@@ -25,14 +13,6 @@ export class TabbarComponent{
   tab1 = Page1Component;
   tab2 = Page2Component;
   tab3 = Page3Component;
-  test : string = "Bonjour";
-  tabLinkList = {
-    "mission" : [
-      new TabLink("Missions","ion-home",this.tab1),
-      new TabLink("Clients","ion-home",this.tab2),
-      new TabLink("Consultants","ion-home",this.tab3)
-    ]
-  };
 
   constructor(private menuService: MenuService) { }
 
