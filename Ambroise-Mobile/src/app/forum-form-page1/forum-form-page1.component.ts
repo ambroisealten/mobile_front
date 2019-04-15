1be1dda52f6ee971084ca3bcbd6890a76382a26d
ThomasDecampAlten <thomas.decamp@alten.com>
2019-04-15 11:09:17 +0200

    Merge

---
+++ Ambroise-Mobile/src/app/forum-form-page1/forum-form-page1.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ons-page',
+  templateUrl: './forum-form-page1.component.html',
+  styleUrls: ['./forum-form-page1.component.scss']
+})
+export class ForumFormPage1Component {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Ambroise-Mobile/src/app/forum-form-page5/forum-form-page5.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ons-page',
+  templateUrl: './forum-form-page5.component.html',
+  styleUrls: ['./forum-form-page5.component.scss']
+})
+export class ForumFormPage5Component {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
